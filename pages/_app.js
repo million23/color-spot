4d936704791b23d85b3d67cf89b82be77d659d98
million23 <chavezgerald23@gmail.com>
2022-12-11 13:08:49 +0800

    updated manifest

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,12 @@ function MyApp({ Component, pageProps }) {
         <meta name="msapplication-starturl" content="/" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+        <link
+          rel="mask-icon"
+          href="/icons/safari-pinned-tab.svg"
+          color="#5bbad5"
         />
 
         <link
@@ -31,6 +36,14 @@ function MyApp({ Component, pageProps }) {
           sizes="512x512"
           href="/logo/512.png"
         />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Color Spot" />
+        <meta
+          property="og:description"
+          content="Color Detection as a Progressive Web App"
+        />
+        <meta property="og:site_name" content="Color Spot" />
       </Head>
 
       <main className="flex justify-center">
