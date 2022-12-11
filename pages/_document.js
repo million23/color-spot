28447e01f10ba081a0c7a287fc11b5fee9d911ef
million23 <chavezgerald23@gmail.com>
2022-12-11 12:39:48 +0800

    updated manifest

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,40 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content="#fff" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+const MyDocument = () => {
+  return (
+    <Html>
+      <Head>
+        <title>Color Spot</title>
+        <meta name="description" content="PWA app for color detection" />
+        {/* <meta name="keywords" content="Keywords" /> */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#fff" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
 
 export default MyDocument;
+
+// class MyDocument extends Document {
+//   render() {
+//     return (
+//       <Html>
+//         <Head>
+//           <link rel="manifest" href="/manifest.json" />
+//           <meta name="theme-color" content="#fff" />
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// }
+
+// export default MyDocument;
